6365a6775874f2bd211e34c78849e0ed58acc7bf
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2020-05-22 22:13:22 +1000

    Latest Commit

--- kivy_app.py
+++ kivy_app.py
@@ -244,18 +244,18 @@ class MainScreen(Screen):
             except:
                 print 'Could not change contrast'
 
-        print
-        print 'Cartoonify Picture'
-        # 1) Edges
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray = cv2.medianBlur(gray, 5)
-        edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
-
-        # 2) Color
-        color = cv2.bilateralFilter(img, 9, 300, 300)
-
-        # 3) Cartoon
-        img = cv2.bitwise_and(color, color, mask=edges)
+        # print
+        # print 'Cartoonify Picture'
+        # # 1) Edges
+        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # gray = cv2.medianBlur(gray, 5)
+        # edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+        #
+        # # 2) Color
+        # color = cv2.bilateralFilter(img, 9, 300, 300)
+        #
+        # # 3) Cartoon
+        # img = cv2.bitwise_and(color, color, mask=edges)
 
         print
         print 'Beginning Remap Color'
